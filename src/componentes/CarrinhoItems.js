5f5ffea454016424fa561a4903e417fa2e0a2d86
Leozbroca <leo.brokka@gmail.com>
2021-11-10 21:53:42 -0300

    Adicionei a parte do carrinho

---
+++ src/componentes/CarrinhoItems.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-bottom:10px;
+  align-items: center;
+  justify-content:space-around;
+
+  p {
+    margin: 0;
+  }
+`
+
+
+class CarrinhoItems extends React.Component {
+  render() {
+    return <ItemContainer>
+      <p>{this.props.item.quantidade}x</p>
+      <p>{this.props.item.nome}</p>
+      <button 
+        onClick={() => this.props.removendoProduto(this.props.item.id)}
+      >Remover
+      </button>
+    </ItemContainer>
+  }
+}
+
+export default CarrinhoItems
